2c31c7a6d16021b68ca7779eea981c64d8fcf7ab
zhangbailong945 <1207549344@qq.com>
2016-12-01 23:48:39 +0800

    update FileManager.php

--- FileManager.php
+++ FileManager.php
@@ -2,11 +2,10 @@
 class FileManager
 {
 	 public $directory; //客户端点开的目录
-     
+     //获取目录
      public function getCurrentDir($currentDir)
      {
          $files =scandir($currentDir); 
-         $list=array();
          $dirList=array();
          $fileList=array();
          foreach($files as $file)
@@ -30,20 +29,45 @@ class FileManager
             }
             
          }
-         $list[]=$dirList;
-         //$list[1]=$fileList;
-         $json=json_encode($dirList);
-         $json=str_replace('[', '{',$json);
-         $json=str_replace(']', '}',$json);
-         echo $json;
+
+         echo json_encode($dirList);
+
          //print_r($dirList);
+     }
+     
+     //获取所有文件
+     public function getCurrentFiles($currentDir)
+     {
+        $files =scandir($currentDir);
+        $fileList=array();
+
+         foreach($files as $file)
+         {
+           if($file=='.')
+           {
+            continue;
+           }
+           if($file=='..')
+           {
+           continue;
+           }
+           $fileList[]=$file;
+         }
+         echo json_encode($fileList);
      }
 }
 
-if(!empty($_POST['directory']))
+if(!empty($_POST['directory'])&&$_POST['action']=='load')
 {
    $directory=$_POST['directory'];
    $obj=new FileManager();
    $obj->getCurrentDir($directory);
+}
+
+if(!empty($_POST['directory'])&&!empty($_POST['opendir'])&&$_POST['action']=='getimage')
+{
+   $directory=$_POST['directory'].'/'.$_POST['opendir'];
+   $obj=new FileManager();
+   $obj->getCurrentFiles($directory);
 }
 
